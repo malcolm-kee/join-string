43661ba1e6de64e9e33831e72d72251ab5ae0c87
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-11-29 04:42:30 +0800

    refactor code to be simpler and remove dependency

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import { isFilledString, isArray, isNumber } from 'typesafe-is';
+type JoinFunction = (...items: any[]) => string;
 
-export const joinString = (delimiter: string) =>
-  function joinFn(...items: Array<any>): string {
+export const joinString = (delimiter: string): JoinFunction =>
+  function joinFn(): string {
     const results: Array<string | number> = [];
 
-    for (let index = 0; index < items.length; index++) {
-      const item = items[index];
+    for (let index = 0; index < arguments.length; index++) {
+      const item = arguments[index];
 
-      if (isNumber(item) || isFilledString(item)) {
+      if (!item && item !== 0) continue;
+
+      const itemType = typeof item;
+
+      if (itemType == 'string' || itemType == 'number') {
         results.push(item);
         continue;
       }
 
-      if (isArray(item)) {
-        results.push(joinFn(...item));
-        continue;
+      if (Array.isArray(item) && item.length > 0) {
+        const innerResult = joinFn.apply(null, item);
+        if (innerResult) {
+          results.push(innerResult);
+        }
       }
     }
 
